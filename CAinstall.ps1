e91185dfacf506e90df091d4f34d72f283682d1e
Shai katzman <88480194+shaikatzman@users.noreply.github.com>
2023-01-24 12:42:38 +0200

    Update CAinstall.ps1

--- CAinstall.ps1
+++ CAinstall.ps1
@@ -3,4 +3,7 @@ Write-Host "installing and configuring CA server on JBH-DC1" -ForegroundColor Ye
 Install-WindowsFeature -Name ADCS-Cert-Authority,ADCS-Web-Enrollment -IncludeManagementTools -IncludeAllSubFeature
 Install-AdcsCertificationAuthority -CAType EnterpriseRootCA -CACommonName "JBH-RootCA" -Force
 Install-AdcsWebEnrollment -Force
-Add-DnsServerResourceRecordA -Name "crl" -ZoneName "JBH.Local" -AllowUpdateAny -IPv4Address "172.16.1.1" -TimeToLive 01:00:00Write-Host " Success Configured CA server on JBH-DC1" -ForegroundColor Green
+Add-DnsServerResourceRecordA -Name "crl" -ZoneName "JBH.Local" -AllowUpdateAny -IPv4Address "172.16.1.1" -TimeToLive 01:00:00
+Invoke-Command -ComputerName HQ-DC1,HQ-SVR2,RTR,HQ-CL1 -ScriptBlock {gpupdate /force}
+Write-Host " Success Configured CA server on JBH-DC1" -ForegroundColor Green
+pause
